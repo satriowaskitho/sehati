6e376fd528d7127e247159a5c348431f178ec368
satriowaskitho <satriowaskitho@gmail.com>
2025-10-28 13:32:26 +0700

    Added server setting to force HTTPS detection for signed URLs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Force HTTPS in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+
+            // Force HTTPS detection for signed URLs
+            request()->server->set('HTTPS', 'on');
+        }
     }
 }
